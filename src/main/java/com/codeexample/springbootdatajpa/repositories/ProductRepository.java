55285166a72b18e49e9fd21b617d7ff9aa07ad77
AamirrH <aamirr.1704@gmail.com>
2025-12-29 11:55:34 +0530

    create custom query methods using JPARepository and Dynamic Query Methods using Different Tags

---
+++ src/main/java/com/codeexample/springbootdatajpa/repositories/ProductRepository.java
@@ -0,0 +1,32 @@
+package com.codeexample.springbootdatajpa.repositories;
+
+import com.codeexample.springbootdatajpa.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+
+    List<ProductEntity> findByQuantityGreaterThanAndPriceGreaterThan(int quantity, BigDecimal price);
+
+    /* Since the return type is singular Product Entity, this method should ensure only
+    a single entity is returned which can be achieved with the help of unique constraints.
+     */
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    /* Creating Custom Complex Queries
+    we have to create a custom JPQL query wherein everything belongs to the java class and then we
+    have to pass it inside @Query message where it will be passed to further hibernate and then it is converted to SQL
+    and then it is executed inside DataBase.
+     */
+    @Query("select e from ProductEntity e where e.price=?1")
+    Optional<ProductEntity> getByPrice(BigDecimal price);
+
+
+}
--- src/test/java/com/codeexample/springbootdatajpa/SpringBootDataJpaApplicationTests.java
+++ src/test/java/com/codeexample/springbootdatajpa/SpringBootDataJpaApplicationTests.java
@@ -1,13 +1,49 @@
 package com.codeexample.springbootdatajpa;
 
+import com.codeexample.springbootdatajpa.entities.ProductEntity;
+import com.codeexample.springbootdatajpa.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SpringBootDataJpaApplicationTests {
 
+    @Autowired // Running Tests
+    ProductRepository productRepository;
+
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void getProductById(){
+        ProductEntity productEntity = productRepository.findById(1L).orElse(null);
+        System.out.println(productEntity.getTitle());
+    }
+    @Test
+    void getProductAfterCreationDate(){
+        LocalDateTime now = LocalDateTime.of(2025,12,28,1,1);
+        List<ProductEntity> productEntities = productRepository.findByCreatedAtAfter(now);
+        System.out.println(productEntities);
+
+    }
+    @Test
+    void getrice(){
+        List<ProductEntity> productEntities = productRepository.findByQuantityGreaterThanAndPriceGreaterThan(2, BigDecimal.valueOf(13.5));
+        System.out.println(productEntities);
+    }
+    @Test
+    void get(){
+        Optional<ProductEntity> productEntity1 = productRepository.findByTitleAndPrice("Oreo",BigDecimal.valueOf(13.4));
+        productEntity1.ifPresent(System.out::println);
+
+    }
+
 
 }
